e1fbf6d1c2b51c8d11d2cc4c3d9956db67a0ac1d
David Gootman <gootman@gmail.com>
2026-01-13 23:36:09 -0800

    Add message id to log context

--- app.py
+++ app.py
@@ -109,33 +109,40 @@ async def on_chat_start():
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    agent: Agent = cl.user_session.get("agent")
+    with logger.contextualize(message_id=message.id):
+        agent: Agent = cl.user_session.get("agent")
 
-    message_history = cl.user_session.get("message_history", [])
+        message_history = cl.user_session.get("message_history", [])
 
-    logger.trace({"message": message.to_dict(), "message_history": message_history})
+        logger.trace(
+            "Message received",
+            message=message.to_dict(),
+            message_history=message_history,
+        )
 
-    stream = agent.run_stream_events(message.content, message_history=message_history)
+        stream = agent.run_stream_events(
+            message.content, message_history=message_history
+        )
 
-    steps: dict[str, cl.Step] = {}
+        steps: dict[str, cl.Step] = {}
 
-    async for event in stream:
-        logger.trace({"event": event})
+        async for event in stream:
+            logger.trace("Event received", event=event)
 
-        if isinstance(event, AgentRunResultEvent):
-            await cl.Message(event.result.output).send()
-            cl.user_session.set("message_history", event.result.all_messages())
+            if isinstance(event, AgentRunResultEvent):
+                await cl.Message(event.result.output).send()
+                cl.user_session.set("message_history", event.result.all_messages())
 
-        elif isinstance(event, FunctionToolCallEvent):
-            step = cl.Step(event.part.tool_name, type="tool", id=event.tool_call_id)
-            step.input = {"input": event.part.args_as_dict()}
+            elif isinstance(event, FunctionToolCallEvent):
+                step = cl.Step(event.part.tool_name, type="tool", id=event.tool_call_id)
+                step.input = {"input": event.part.args_as_dict()}
 
-            steps[event.tool_call_id] = step
+                steps[event.tool_call_id] = step
 
-            await step.__aenter__()
+                await step.__aenter__()
 
-        elif isinstance(event, FunctionToolResultEvent):
-            step = steps[event.tool_call_id]
-            step.output = {"output": event.result.model_response_object()}
+            elif isinstance(event, FunctionToolResultEvent):
+                step = steps[event.tool_call_id]
+                step.output = {"output": event.result.model_response_object()}
 
-            await step.__aexit__(None, None, None)
+                await step.__aexit__(None, None, None)
