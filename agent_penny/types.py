f1eea038ef621a44c3a368cab3252095e3a6d54f
David Gootman <gootman@gmail.com>
2026-01-17 20:51:47 -0800

    Structured data types

---
+++ agent_penny/types.py
@@ -0,0 +1,30 @@
+from datetime import date, datetime
+from typing import NotRequired, TypedDict
+
+
+class Calendar(TypedDict):
+    id: str
+    name: str
+    description: NotRequired[str]
+
+
+class CalendarEvent(TypedDict):
+    id: str
+    name: str
+    description: NotRequired[str]
+    start_time: datetime | date
+    end_time: datetime | date
+    calendar_id: str
+
+
+MailMessage = TypedDict(
+    "MailMessage",
+    {
+        "id": str,
+        "subject": str,
+        "from": str,
+        "to": str,
+        "received": datetime,
+        "content": str,
+    },
+)
