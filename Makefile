6f1573923595d6048a7403106e7f303674339d57
David Gootman <gootman@gmail.com>
2026-02-16 08:12:06 -0800

    Pytest for audio support

--- Makefile
+++ Makefile
@@ -1,18 +1,21 @@
-.PHONY: release build review readme sync
+.PHONY: all build readme review sync test
 
-release: build readme
-
-sync:
-	uv sync
+all: build test
 
 build: sync
 	uv run ruff check
 	uv run mypy app.py agent_penny
 
-review:
-	gemini -p "Review the staged changes using the code-reviewer skill"
-
 readme: README.md
 
 README.md: app.py agent_penny/*.py agent_penny/**/*.py
 	gemini -p "Update the README.md file using the update-readme skill" -m gemini-3-pro-preview --approval-mode auto_edit
+
+review:
+	gemini -p "Review the staged changes using the code-reviewer skill"
+
+sync:
+	uv sync
+
+test:
+	uv run pytest
---
+++ tests/test_audio.py
@@ -0,0 +1,61 @@
+import os
+from pathlib import Path
+import sys
+import wave
+
+import pytest
+from loguru import logger
+
+logger.remove()
+logger.add(sys.stderr, serialize=True)
+
+
+async def transcribe(file: str) -> str:
+    from agent_penny.audio import StreamingTranscriber
+
+    os.environ["WHISPER_MODEL"] = "small.en"
+    with wave.open(file, "rb") as f:
+        audio = f.readframes(sys.maxsize)
+
+    transcriber = StreamingTranscriber(16000)
+    text = await transcriber.add_chunk(audio)
+    if text is None:
+        logger.debug("Chunk wasn't transcribed immediately")
+        text = await transcriber.transcribe()
+
+    logger.debug("Transcription", text=text)
+
+    assert text is not None
+
+    return text
+
+
+@pytest.mark.asyncio
+async def test_speech_to_text():
+    text = await transcribe("tests/test_audio.wav")
+
+    assert "this is a test" in text.lower()
+    assert "text" in text.lower()
+    assert "speech" in text.lower()
+    assert "system" in text.lower()
+
+
+@pytest.mark.asyncio
+async def test_end_to_end(tmp_path: Path):
+    from agent_penny.audio import text_to_speech
+
+    wave_file = str(tmp_path / "output.wav")
+
+    speech = text_to_speech("Hello! My name is Agent Penny")
+    chunk = next(speech)
+    with wave.open(wave_file, "wb") as f:
+        f.setparams((1, 2, 16000, 0, "NONE", "NONE"))
+        f.writeframes(chunk)
+    assert next(speech, None) is None
+
+    text = await transcribe(wave_file)
+
+    assert text is not None
+    assert "hello" in text.lower()
+    assert "name is " in text.lower()
+    assert "agent penny" in text.lower()
