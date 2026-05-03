227ebdcaf9e724dc119e4b4f57dd125feb0fa1b3
David Gootman <gootman@gmail.com>
2026-05-03 07:01:08 +0000

    Defer import for Perplexity test

--- tests/test_perplexity.py
+++ tests/test_perplexity.py
@@ -3,14 +3,14 @@ import os
 import pytest
 from loguru import logger
 
-from agent_penny.tools.perplexity import perplexity
-
 pytestmark = pytest.mark.skipif(
     not os.environ.get("PERPLEXITY_API_KEY"), reason="PERPLEXITY_API_KEY not set"
 )
 
 
 def test_perplexity():
+    from agent_penny.tools.perplexity import perplexity
+
     results = perplexity("Who is Miss Moneypenny?")
 
     logger.debug("Results", results=results.to_dict())
